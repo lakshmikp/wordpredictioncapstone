eebd346796df8078dd0cde77d548e17c4df56395
Lakshmi K P <lakshmikp@yahoo.com>
2016-01-16 21:55:30 -0800

    updated

--- process.R
+++ process.R
@@ -1,4 +1,6 @@
+memory.limit(size=100000)
 setwd("D://Personal//Data Science//git//Capstone//data//")
+source("..//spell-correction.R")
 #file.split("D://Personal//Data Science//git//Capstone//data-original//", size = 1024, same.dir = FALSE, verbose = TRUE, suf = "part", win = TRUE)
 
 library(quanteda)
@@ -10,7 +12,7 @@ library(stringr)
 
 nGramIndex <- 5
 minimumFrequency <- c(0,20,10,5,2,1)
-numberOfLines <- 100
+numberOfLines <- 200
 
 createNGram <- function(x, m){
   tdmNgram <- dfm(unlist(x), ngrams = m, verbose = FALSE, stem = TRUE)
@@ -19,6 +21,19 @@ createNGram <- function(x, m){
   #Remove non-alpha terms
   pt <- "[^_[:alpha:]]"
   d[grep(x=d$Term, pattern=pt, invert=TRUE)]
+}
+
+getCorrectSpelling <- function(x, l){
+  print(paste0(Sys.time(), "- Inside function getCorrectSpelling"))
+  n <- length(x)
+  for (i in 1:n){
+    res <- subset(l, Word==x[i])$Correction
+    if (! is.null(res)){
+      x[i] <- head(res,1)
+    }
+    
+  }
+  return(x)
 }
 
 
@@ -63,62 +78,28 @@ for (m in 2:nGramIndex){
   
   dt[[m]] <- mutate(dt[[m]], Next= word(Term, start=-1,end=-1, sep=fixed('_')))
   dt[[m]] <- mutate(dt[[m]], Input = word(Term, start=1, end=(N-1), sep=fixed('_')))
+  
+  
+  #Spell corrections
+  splist <- data.table(Word=character(0), Correction=character(0))
+  uniqueWords <- unique(dt[[m]]$Next)
+  for (w in uniqueWords){
+    splist <- rbind(splist, data.table(Word=w, Correction=correct(w)))
+    
+  }
+  dt[[m]] <- mutate(dt[[m]], Next=getCorrectSpelling(Next, splist))
+  
+  
+
   fname <- paste0("..//word-prediction//dt", m, ".Rds")
   saveRDS(dt[[m]], file=fname)
 }
 
 
-# Spell corrections
-# Profanity
-# score based on length of word  example: "conference call" (in this case 3gram shd get higher priority irrespective of the frequency)
-# Note on UI about English only
 
 
 
-library(tm)
-library(SnowballC)
-
-stemDocumentfix <- function(x)
-{
-  t <- stemDocument(unlist(strsplit(as.character(x), " ")))
-  r <- ""
-  for (i in 1:length(t)){
-    r <- paste(r, t[i])
-  }
-  str_trim(r)
-}
 
 
-getNextWord <- function(x){
-  print(x)
-  x <- str_trim(stripWhitespace(removeNumbers(stemDocumentfix(removePunctuation(tolower(x))))))
-  x <- gsub(x=x, pattern=" ", replacement="_")
-  print(x)
-  res <- data.table(Next=character(0), N=integer(0), Frequency=integer(0), Input=character(0))
-  
-  try(
-    for (k in nGramIndex:2){
-      st <- word(x, -1*(k-1), -1, sep=fixed('_'))
-      print(paste("Looking for", st))
-      print(head(dt[[k]]))
-      result <- 
-        dt[[k]] %>%
-        filter (Input==st)%>%
-        select(Next, Frequency, N, Input)
-      
-      res <- rbind(res, result)
-      
-    }, silent = TRUE)
-  
-  if (nrow(res)==0){
-    print ("the")
-  }else{
-    res <- mutate(res, Score=ifelse(Next  %in% stopwords(), yes=Frequency*N, no=Frequency*(10*N)))
-    print(arrange(res, desc(Score)))
-    print(head(arrange(res, desc(Score)),1)$Next)
-  }
-  
-}
+                    
 
-#TBD
-#Spelling corrections
