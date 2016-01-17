f33b64ab2ab0d7eb9faace8275050305c0611f62
Lakshmi K P <lakshmikp@yahoo.com>
2016-01-16 21:56:03 -0800

    updated

--- word-prediction/server.R
+++ word-prediction/server.R
@@ -6,7 +6,7 @@ library(stringr)
 library(tm)
 library(SnowballC)
 
-nGramIndex <- 4
+nGramIndex <- 5
 dt <- list()
 
 for (m in 2:nGramIndex){
@@ -29,17 +29,19 @@ stemDocumentfix <- function(x)
 }
 
 getNextWord <- function(x){
+  if (x=="")  {
+    return("")
+  }
   print(x)
   x <- str_trim(stripWhitespace(removeNumbers(stemDocumentfix(removePunctuation(tolower(x))))))
   x <- gsub(x=x, pattern=" ", replacement="_")
-  print(x)
   res <- data.table(Next=character(0), N=integer(0), Frequency=integer(0), Input=character(0))
   
   try(
     for (k in nGramIndex:2){
       st <- word(x, -1*(k-1), -1, sep=fixed('_'))
-      print(paste("Looking for", st))
-      print(head(dt[[k]]))
+      
+      
       result <- 
         dt[[k]] %>%
         filter (Input==st)%>%
@@ -50,17 +52,20 @@ getNextWord <- function(x){
     }, silent = TRUE)
   
   if (nrow(res)==0){
-    print ("the")
+    print ("the,and,that")
   }else{
-    res <- mutate(res, Score=ifelse(Next  %in% stopwords(), yes=Frequency*N, no=Frequency*(10*N)))
-    print(arrange(res, desc(Score)))
-    print(head(arrange(res, desc(Score)),1)$Next)
+    #res <- mutate(res, Score=ifelse(Next  %in% stopwords(), yes=Frequency*N, no=Frequency*(10*N)))
+    res <- res[order(-N, -Frequency)]
+    print(res)
+    #easi, import, excit,happi, peopl, amaz
+    print(head(unique(res$Next),3))
   }
   
 }
 
 shinyServer(
   function(input, output) {
-       output$nextWord <- reactive({getNextWord(input$inputWords)})
-     }
+    output$nextWord <- reactive({getNextWord(input$inputWords)})
+    
+  }
 )
